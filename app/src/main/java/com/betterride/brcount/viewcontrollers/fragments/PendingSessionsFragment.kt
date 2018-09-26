dda02d054253cd55a586638a4b8bfa9b084a9ec5
Daragonor <daniel.aragon.0235@hotmail.com>
2018-09-26 03:02:21 -0500

    recycler view finished, needs header for section

--- app/src/main/java/com/betterride/brcount/viewcontrollers/fragments/PendingSessionsFragment.kt
+++ app/src/main/java/com/betterride/brcount/viewcontrollers/fragments/PendingSessionsFragment.kt
@@ -2,11 +2,20 @@ package com.betterride.brcount.viewcontrollers.fragments
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.GridLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.androidnetworking.error.ANError
 
 import com.betterride.brcount.R
+import com.betterride.brcount.models.Session
+import com.betterride.brcount.network.BRApi
+import com.betterride.brcount.network.SessionsResponse
+import com.betterride.brcount.viewcontrollers.adapters.CountingSessionsAdapter
+import kotlinx.android.synthetic.main.fragment_pending_sessions.view.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -15,10 +24,43 @@ private const val ARG_PARAM2 = "param2"
 
 class PendingSessionsFragment : Fragment() {
 
+    var sessions = ArrayList<Session>()
+    lateinit var sessionsRecyclerView: RecyclerView
+    lateinit var sessionsAdapter: CountingSessionsAdapter
+    lateinit var sessionsLayoutManager: RecyclerView.LayoutManager
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pending_sessions, container, false)
+        val view = inflater.inflate(R.layout.fragment_pending_sessions, container, false)
+
+        sessionsRecyclerView = view.sessionsRecyclerView
+        sessionsAdapter = CountingSessionsAdapter(sessions, view.context)
+        sessionsLayoutManager = GridLayoutManager(view.context, 1)
+        sessionsRecyclerView.adapter = sessionsAdapter
+        sessionsRecyclerView.layoutManager = sessionsLayoutManager
+
+        BRApi.requestHeadlines(
+                { response -> handleResponse(response)},
+                { error -> handleError(error)})
+
+        return view
+    }
+
+    private fun handleResponse(response: SessionsResponse?) {
+        val status = response!!.status
+        if (status.equals("error", true)) {
+            Log.d("CatchUp", response.message)
+            return
+        }
+
+        sessions = response.sessions!!
+        Log.d("BRCount", "Found ${sessions.size} articles")
+        sessionsAdapter.sessions = sessions
+        sessionsAdapter.notifyDataSetChanged()
+    }
+
+    private fun handleError(anError: ANError?) {
+        Log.d("BRCount", anError!!.message)
     }
 
 
